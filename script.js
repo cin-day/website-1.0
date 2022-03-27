917c889d373f8c8209829221c5610392fe744c10
Cindy Wang <cinwan23@gmail.com>
2022-03-26 22:20:37 -0400

    Finished typing and deleting animation

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function waitDelay(ms) {
     });
 }
 
-async function type(sentence, delay = 100) {
+async function typeAnim(sentence, delay = 100) {
     const letters = sentence.split("");
     let len = letters.length;
     for (let i = 0; i < len; i++) {
@@ -21,4 +21,36 @@ async function type(sentence, delay = 100) {
     }
 }
 
-type("Hey, over here!");
+async function deleteAnim(delay = 20) {
+    const sentence = textElement.textContent;
+    // const sentence = textElement.html();
+    const letters = sentence.split("");
+    let len = letters.length;
+    for (let i = 0; i < len; i++) {
+        await waitDelay(delay);
+        letters.pop();
+        textElement.textContent = letters.join("");
+        // textElement.html(letters.join(""));
+    }
+}
+
+// $( document ).ready(async function() {
+    // typeAnim("Hey, over here!");
+    // waitForMs(2000);
+    // deleteAnim();
+// });
+
+async function type() {
+    let i = 0;
+    let len = sentences.length;
+    while(true) {
+        await typeAnim(sentences[i]);
+        await waitDelay(1500);
+        await deleteAnim();
+        await waitDelay(500);
+        i++;
+        if (i >= len) {i = 0;}
+    }
+}
+
+type();
